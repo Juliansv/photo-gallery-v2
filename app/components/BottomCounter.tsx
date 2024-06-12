442c65cb418a4804aa50c3d7b8a2ee545e93077c
Juliansv <juliansv22@gmail.com>
2024-06-12 21:38:42 +0200

    Create component

---
+++ app/components/BottomCounter.tsx
@@ -0,0 +1,14 @@
+const BottomCounter = () => {
+	return (
+		<section
+			className="absolute h-full w-full opacity-0"
+			id="bottom-counter"
+		>
+			<div className="w-40 absolute bottom-20 left-1/2">
+				<span id="counter">1</span> - 10
+			</div>
+		</section>
+	);
+};
+
+export default BottomCounter;
---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+const HeroSection = () => {
+	return (
+		<section id="hero-section" className="w-[50vw]">
+			<div className="h-screen flex flex-col justify-center gap-4 p-10 w-full">
+				<h1 className="text-5xl font-bold">Emiliano Suarez Vivas</h1>
+				<p className="text-xl">Estos son mis rollos</p>
+			</div>
+		</section>
+	);
+};
+
+export default HeroSection;
