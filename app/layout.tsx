61ca70689e4bc5764ea6c44f07f39527597d5105
Juliansv <juliansv22@gmail.com>
2024-06-17 23:06:42 +0200

    update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Photo Gallery v2",
+	title: "Emilio's Photos",
 	description: "Website to showcase photos",
 };
 
