b0471e81ed4dfe3be2fc4dedcb922a3809db7ea5
Juliansv <juliansv22@gmail.com>
2024-06-27 21:48:25 +0200

    Update with Emi suggestions

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -2,8 +2,8 @@ const HeroSection = () => {
 	return (
 		<section id="hero-section" className="w-[50vw]">
 			<div className="h-screen flex flex-col justify-center gap-4 p-10 w-full">
-				<h1 className="text-5xl font-bold">Emiliano Suarez Vivas</h1>
-				<p className="text-xl">Estos son mis rollos</p>
+				<h1 className="text-5xl font-bold">Emiliano</h1>
+				<p className="text-xl">Film photos</p>
 			</div>
 		</section>
 	);
